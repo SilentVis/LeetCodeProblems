67ed9b4f261c113e60811b7cc7cfe0ff00306dc2
Serhii Tereshchenko <steres@softserveinc.com>
2023-05-29 15:05:42 +0300

    solved: https://leetcode.com/explore/interview/card/microsoft/30/array-and-strings/179/

---
+++ LeetCodeTasks.Tests/ValidParenthesisTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCodeTasks.Tests
+{
+    public class ValidParenthesesTests
+    {
+        private ValidParentheses.Solution _solution;
+
+
+        public ValidParenthesesTests()
+        {
+            _solution = new ValidParentheses.Solution();
+        }
+
+        [Theory]
+        [InlineData("{}",true)]
+        [InlineData("()",true)]
+        [InlineData("[]",true)]
+        [InlineData("{}()[]",true)]
+        [InlineData("{([])}",true)]
+        [InlineData("{(})",false)]
+        [InlineData("[",false)]
+        [InlineData("((",false)]
+        [InlineData("){",false)]
+        public void IsValid_ShouldReturnCorrectResult(string input, bool expectedResult)
+        {
+            var actualResult = _solution.IsValid(input);
+
+            actualResult.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ LeetCodeTasks/ValidParentheses.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCodeTasks
+{
+    public class ValidParentheses
+    {
+        public class Solution
+        {
+
+            private static Dictionary<char, char> pairs = new()
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' },
+            };
+
+            private Stack<char> parenthesisStack = new();
+
+            public bool IsValid(string inputString)
+            {
+                var currentOpenedParenthesis = char.MinValue;
+
+
+                if (inputString.Length % 2 != 0)
+                {
+                    return false;
+                }
+
+                foreach (var symbol in inputString)
+                {
+                    if (symbol is '(' or '{' or '[')
+                    {
+                        if (currentOpenedParenthesis != char.MinValue)
+                        {
+                            parenthesisStack.Push(currentOpenedParenthesis);
+                        }
+
+                        currentOpenedParenthesis = symbol;
+                    }
+                    else
+                    {
+                        if (currentOpenedParenthesis == char.MinValue)
+                        {
+                            return false;
+                        }
+
+                        var expectedClosing = pairs[currentOpenedParenthesis];
+                        if (expectedClosing != symbol)
+                        {
+                            return false;
+                        }
+
+                        currentOpenedParenthesis = parenthesisStack.Any() ? parenthesisStack.Pop() : char.MinValue;
+                    }
+                }
+
+                return !parenthesisStack.Any();
+            }
+        }
+    }
+}
